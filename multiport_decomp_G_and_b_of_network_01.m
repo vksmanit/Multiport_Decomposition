02f0eeb863d0acdd6b3bdd5613e3963037de7a3c
vksmanit <vksmanit@gmail.com>
2018-06-18 02:25:57 +0530

    working with ckt_01 and ckt_03

--- multiport_decomp_G_and_b_of_network_01.m
+++ multiport_decomp_G_and_b_of_network_01.m
@@ -12,10 +12,12 @@ function [i_p1, G_p1] =  multiport_decomp_G_and_b_of_network_01(cktnetlist)
    %i_p1 = A2e_n1\(-A2g_n1 * G_n1 * A1g_n1' * v_n1 - A2g_n1 * J_n1);
    %v_n1 = equation_5_matrix \ (- A1g_n1 * G_n1 * A2g_n1' * inv(A2e_n1')); 
    %G_p1 = inv(A2e_n1) * (-A2g_n1 * G_n1 *(A1g_n1' * v_n1 + A2g_n1' * inv(A2e_n1')));
-
+    size_of_A2e_n1 = size(A2e_n1);
+    v_E = eye(size_of_A2e_n1);
     M = A1g_n1 * G_n1 * A1g_n1';
-    v_n1 = M\(-A1g_n1 * G_n1 * A2g_n1' * inv(A2e_n1'));
-    G_p1 = A2e_n1 \ (-A2g_n1 * G_n1 *(A1g_n1' * v_n1 + A2g_n1' * inv(A2e_n1')));
+    v_n1 = M\(-A1g_n1 * G_n1 * A2g_n1' * (A2e_n1'\v_E));
+    %G_p1 = A2e_n1 \ (-A2g_n1 * G_n1 *(A1g_n1' * v_n1 + A2g_n1' * inv(A2e_n1')));
+    G_p1 = A2e_n1 \ (-A2g_n1 * G_n1 *(A1g_n1' * v_n1 + A2g_n1' * (A2e_n1'\v_E)));
     %%%% for short ckt current %%%
     v_n1 = M\(-A1g_n1 * J_n1);
     i_p1 = A2e_n1 \ (-A2g_n1 * G_n1 * A1g_n1' * v_n1 - A2g_n1 *  J_n1); 
